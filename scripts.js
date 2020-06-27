142abad412b1d6946e219695b1e2456fa88cb217
vijay4004 <66413115+vijay4004@users.noreply.github.com>
2020-06-27 23:49:59 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,3 @@
-$(document).ready(function () {
-    $("#mycarousel").carousel({ interval: 2000 });
-    $("#carouselButton").click(function () {
-        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-            $("#mycarousel").carousel('pause');
-            $("#carouselButton").children("span").removeClass('fa-pause');
-            $("#carouselButton").children("span").addClass('fa-play');
-        }
-        else if ($("#carouselButton").children("span").hasClass('fa-play')) {
-            $("#mycarousel").carousel('cycle');
-            $("#carouselButton").children("span").removeClass('fa-play');
-            $("#carouselButton").children("span").addClass('fa-pause');
-        }
-    });
-});
-
-$("#reserveBtn").click(function () {
-    $('#reservationModal').modal('toggle')
-});
-$("#loginBtn").click(function () {
-    $('#loginModal').modal('toggle')
-});
-
-
 document.getElementById("plot").onclick = function() {myFunction()};
 
 function myFunction() {
@@ -67,6 +43,7 @@ function myFunction() {
             }
         }
         Plotly.newPlot('myChart', data)
+        console.log('The number of bounces id: ' + iter)
       }
       catch (err) {
         console.error(err)
